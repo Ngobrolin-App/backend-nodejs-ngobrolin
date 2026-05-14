600892cdc9d94aad0b06bf137a9b610a0432ee45
Yudha <yudhah52@gmail.com>
2026-05-14 17:33:19 +0700

    fix: smtp

--- src/utils/email.js
+++ src/utils/email.js
@@ -3,7 +3,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async ({ to, subject, html }) => {
     // Create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
