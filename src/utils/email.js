c649a0997c2cca61caab290e358bb221fcf84e6c
Yudha <yudhah52@gmail.com>
2026-05-14 16:58:48 +0700

    fix: smtp

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,11 +4,15 @@ const sendEmail = async ({ to, subject, html }) => {
     // Create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+        port: parseInt(process.env.SMTP_PORT) || 587,
+        secure: process.env.SMTP_SECURE === 'true', // true for 465, false for 587
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
+        },
+        tls: {
+            // Berguna jika server menolak sertifikat TLS
+            rejectUnauthorized: false
         }
     });
 
